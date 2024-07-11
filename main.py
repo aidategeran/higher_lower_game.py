eead9fa7f50e01d2bb98c667dedfe753ad45ed56
Aida Tegeran <aidategeran@Aidas-Laptop.local>
2024-07-11 13:43:06 -0500

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from game_data import data
 from art import logo
 
-import random 
 
+#Display art
 print(logo)
+#Generate a random account from the game data
 
-account_a=random.choice(data)
-account_b=random.choice(data)
-if account_a==account_b:
-    account_b=random.choice(data)
+# ask user for a guess
 
+# check if users is correct 
+# get follower count of each account 
+# use if statement to check if user is correct 
 
-print(account_a)
-print(account_b)
+# give user feedback on their guess
 
+#score keeping 
+#make the game repeatable 
+#Making account at position B become the next account at position A.
+# clear the screeb between rounds.
